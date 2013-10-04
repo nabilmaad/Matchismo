ad439b867334f83393aae79fb4d60700876ef4df
Nabil Maadarani <Nabil@Nabils-MacBook-Pro.local>
2013-10-03 23:35:27 -0700

    I have a card that flips between the Apple logo and the Ace of clubs (View & Controller only

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -14,16 +14,10 @@
 
 @implementation CardGameViewController
 
-- (void)viewDidLoad
+- (IBAction)flipCard:(UIButton *)sender
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    sender.selected = !sender.isSelected;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
